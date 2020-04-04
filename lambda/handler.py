33aaf5644a821b87ad23efd302bf39d231da4b1f
Nick Paz <nicholasmpaz@gmail.com>
2020-04-04 12:13:59 -0700

    finish frontend logic

--- lambda/handler.py
+++ lambda/handler.py
@@ -6,7 +6,9 @@ from tempfile import TemporaryDirectory
 from subprocess import run, check_output, CalledProcessError
 import boto3
 
+API_ENDPOINT = "https://api.blogformation.net"
 CLONE_ERROR = "Could not clone this repository. It is either not public, or doesn't exist."
+
 
 class ChangedFile():
 
@@ -17,6 +19,7 @@ class ChangedFile():
     def __str__(self):
         return self.name
 
+
 class Step():
 
     def __init__(self, message: str, changes: list):
@@ -26,17 +29,19 @@ class Step():
     def __str__(self):
         return self.message
 
+
 def _send_to_connection(data, event):
-    gatewayapi = boto3.client("apigatewaymanagementapi",
-                              endpoint_url="https://api.blogformation.net")
+    gatewayapi = boto3.client("apigatewaymanagementapi", endpoint_url=API_ENDPOINT)
     return gatewayapi.post_to_connection(ConnectionId=event["requestContext"].get("connectionId"),
                                          Data=json.dumps(data).encode('utf-8'))
+
 
 def _send_error(error, event):
     _send_to_connection({
         'message': 'error',
         'data': error
     }, event)
+
 
 def my_handler(event, context):
 
@@ -46,13 +51,15 @@ def my_handler(event, context):
 
     with TemporaryDirectory() as tmpdir:
         try:
-            clone_process = run(['git', 'clone', repo, tmpdir], capture_output=True, check=True)
+            clone_process = run(['git', 'clone', repo, tmpdir],
+                                capture_output=True, check=True)
         except CalledProcessError as e:
             _send_error(CLONE_ERROR, event)
             raise e
         try:
-            commits_process = run(['git', '-C', tmpdir, "rev-list", "master", "--reverse"], capture_output=True, check=True)
-        except CalledProcessError as e: 
+            commits_process = run(['git', '-C', tmpdir, "rev-list",
+                                   "master", "--reverse"], capture_output=True, check=True)
+        except CalledProcessError as e:
             raise e
         commits_raw = commits_process.stdout.decode('utf-8')
         commits = commits_raw.split('\n')[:-1]
@@ -63,21 +70,25 @@ def my_handler(event, context):
             current_commit = commits[i]
             previous_commit = commits[i-1]
             try:
-                commit_message_process = run(['git', '-C', tmpdir, 'show', '-s', '--format=%B', current_commit], capture_output=True, check=True)
-            except CalledProcessError as e: 
+                commit_message_process = run(
+                    ['git', '-C', tmpdir, 'show', '-s', '--format=%B', current_commit], capture_output=True, check=True)
+            except CalledProcessError as e:
                 raise e
-            commit_message = commit_message_process.stdout.decode('utf-8').strip()
+            commit_message = commit_message_process.stdout.decode(
+                'utf-8').strip()
             try:
-                changed_files_process = run(['git', '-C', tmpdir, 'diff-tree', '-r', '--no-commit-id', '--name-only', current_commit], capture_output=True, check=True)
-            except CalledProcessError as e: 
+                changed_files_process = run(['git', '-C', tmpdir, 'diff-tree', '-r', '--no-commit-id',
+                                             '--name-only', current_commit], capture_output=True, check=True)
+            except CalledProcessError as e:
                 raise e
             changed_files_raw = changed_files_process.stdout.decode('utf-8')
             changed_files = changed_files_raw.split('\n')[:-1]
             changes = []
             for f in changed_files:
                 try:
-                    file_diff_process = run(['git', '-C', tmpdir,'diff', '--color-words', previous_commit, current_commit, f], capture_output=True, check=True)
-                except CalledProcessError as e: 
+                    file_diff_process = run(['git', '-C', tmpdir, 'diff', '--color-words',
+                                             previous_commit, current_commit, f], capture_output=True, check=True)
+                except CalledProcessError as e:
                     raise e
                 file_diff = file_diff_process.stdout.decode('utf-8')
                 changes.append(ChangedFile(f, file_diff))
@@ -95,7 +106,8 @@ def my_handler(event, context):
         blog_str += 'MESSAGE: ' + step.message + '\n\n'
         for change in step.changes:
             blog_str += '\tCHANGE: ' + change.name + '\n\n'
-            blog_str += '\n'.join(['\t\t' + line for line in change.diff.split('\n')]) + '\n'
+            blog_str += '\n'.join(['\t\t' +
+                                   line for line in change.diff.split('\n')]) + '\n'
         blog_str += '=========================================================\n\n'
 
     if not test:
